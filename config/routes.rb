773aa41dae61ad1b52fb2db52e57a0cd2706bcc2
YuIshikawa <166091720+29yu03@users.noreply.github.com>
2024-05-30 09:04:03 +0000

    課題5完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#top"
   get 'home/about', to: 'home#about', as: 'about'
+  get "search" => "searches#search"
 
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorite, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :index, :update,] do
+  resources :users, only: [:show, :edit, :index, :update] do
     member do
       get :followings, :followers
       post :follow, to: 'relationships#create'
